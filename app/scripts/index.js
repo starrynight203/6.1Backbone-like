16429f79948f527921ab62b39f0d7f53f196a142
Hannah Smith <hannahsmith@Hannahs-MacBook-Pro.local>
2016-03-07 21:26:29 -0500

    added custom .toJSON method

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,20 +2,38 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 
 var Like = Backbone.Model.extend({ // ... like: function() { // do some stuff } });
-  initialize: function(){
-    $('.like').text('0 likes');
+
+  defaults: {
+    'likes': 0,
+    'buttonLabel': 'Like'
   },
+
+  initialize: function(){
+    $('.like').text('0');
+    $('.likeLabel').text('Likes');
+  },
+
   addLikes: function(){
     this.set({'likes': this.get('likes') + 1});
     //console.log(this.get('likes'));
     return this.get('likes');
+  },
+
+  toJSON: function(){
+    if (this.get('likes') != 1){
+    this.set('buttonLabel', "Likes")
+    }
+    // return toJSON original functionality
+    return Backbone.Model.prototype.toJSON.call(this);
   }
+
 });
 
-var like = new Like({'likes': 0});
+var like = new Like();
 
-
-$('.like').on('click', function(){
+$('.myButton').on('click', function(){
   var storedValue = like.addLikes();
-  $('.like').text(storedValue + ' likes');
+  $('.like').text(storedValue);
+  var labelValue = like.toJSON();
+  $('.likeLabel').text(labelValue.buttonLabel)
 })
