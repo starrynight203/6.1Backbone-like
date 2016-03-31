27c9ed80fe9ab7ced4465d015f42e4a23928408f
Hannah Smith <starrynight203@msn.com>
2016-03-31 11:32:54 -0400

    moved model to actual model file

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,33 +1,6 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
-
-var Like = Backbone.Model.extend({ // ... like: function() { // do some stuff } });
-
-  defaults: {
-    'likes': 0,
-    'buttonLabel': 'Like'
-  },
-
-  initialize: function(){
-    $('.like').text('0');
-    $('.likeLabel').text('Likes');
-  },
-
-  addLikes: function(){
-    this.set({'likes': this.get('likes') + 1});
-    //console.log(this.get('likes'));
-    return this.get('likes');
-  },
-
-  toJSON: function(){
-    if (this.get('likes') != 1){
-    this.set('buttonLabel', 'Likes');
-    }
-    // return toJSON original functionality
-    return Backbone.Model.prototype.toJSON.call(this);
-  }
-
-});
+var Like = require('./models/like');
 
 var like = new Like();
 
@@ -36,4 +9,4 @@ $('.myButton').on('click', function(){
   $('.like').text(storedValue);
   var labelValue = like.toJSON();
   $('.likeLabel').text(labelValue.buttonLabel)
-})
+});
--- app/scripts/models/like.js
+++ app/scripts/models/like.js
@@ -3,4 +3,30 @@ var $ = require('jquery');
 
 var Like = Backbone.Model.extend({ // ... like: function() { // do some stuff } });
 
+  defaults: {
+    'likes': 0,
+    'buttonLabel': 'Like'
+  },
+
+  initialize: function(){
+    $('.like').text('0');
+    $('.likeLabel').text('Likes');
+  },
+
+  addLikes: function(){
+    this.set({'likes': this.get('likes') + 1});
+    //console.log(this.get('likes'));
+    return this.get('likes');
+  },
+
+  toJSON: function(){
+    if (this.get('likes') != 1){
+    this.set('buttonLabel', 'Likes');
+    }
+    // return toJSON original functionality
+    return Backbone.Model.prototype.toJSON.call(this);
+  }
+
 });
+
+module.exports = Like;
